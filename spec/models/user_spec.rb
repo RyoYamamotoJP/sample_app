0f5d509cc6dcdc8ae7e465e270563a6ed9b3372e
Ryo Yamamoto <ryoyamamoto.jp@gmail.com>
2013-07-23 23:41:05 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  
+
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
-  
+
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-  
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -60,7 +61,7 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-    
+
     it { should_not be_valid }
   end
 
@@ -96,5 +97,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
